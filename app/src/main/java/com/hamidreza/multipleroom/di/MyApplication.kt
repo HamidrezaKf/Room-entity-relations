e0c83a0dc8148cf6ffbce2e18a10ed90ea24baf2
Hamidrezakf <hrk1fard@gmail.com>
2021-01-02 10:21:46 +0330

    add dependencies

---
+++ app/src/main/java/com/hamidreza/multipleroom/di/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.hamidreza.multipleroom.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+}
---
+++ app/src/main/java/com/hamidreza/multipleroom/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hamidreza.multipleroom.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hamidreza.multipleroom.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
